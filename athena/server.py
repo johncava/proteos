565663bd7ecaace06f0e4ed7661f8e355f56bc2f
johncava <jcava@asu.edu>
2017-07-21 23:25:14 -0700

    added flask-cors for allowing cors

--- athena/server.py
+++ athena/server.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask_cors import CORS, cross_origin
 from utility import *
 
 app = Flask(__name__)
+CORS(app)
 
 #Initialize ProtVec
 model = initialize()
